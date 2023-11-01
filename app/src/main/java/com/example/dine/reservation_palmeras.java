2ca421835cf984e35456b5e538247d1646f20564
aenjeldelosreyes <aenjeldlsrys@gmail.com>
2023-11-01 21:38:31 +0800

    first_draft

---
+++ app/src/main/java/com/example/dine/reservation_palmeras.java
@@ -0,0 +1,14 @@
+package com.example.dine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class reservation_palmeras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reservation_palmeras);
+    }
+}
